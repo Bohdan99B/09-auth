21dbc602d1f956a3677255c8857d4fd125f18424
Bohdan99B <bozheikob@gmail.com>
2026-02-07 18:53:16 +0200

    fix for Versel

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 const isBrowser = typeof window !== 'undefined';
 
-const origin = isBrowser
-  ? window.location.origin
-  : process.env.NEXT_PUBLIC_API_URL
-    ?? (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000');
+const serverOrigin = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000';
+
+const origin = isBrowser ? window.location.origin : serverOrigin;
 
 const baseURL = `${origin}/api`;
 
