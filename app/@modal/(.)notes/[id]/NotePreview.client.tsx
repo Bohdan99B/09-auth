1574e827d91a7c0895117b63531e11e9855d47d3
Bohdan99B <bozheikob@gmail.com>
2026-02-09 04:49:55 +0200

    last update

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
 import Modal from '@/components/Modal/Modal';
-import NotePreview from '@/components/NotePreview/NotePreview';
+import { fetchNoteById } from '@/lib/api/clientApi';
+import css from '@/components/NotePreview/NotePreview.module.css';
 
 interface NotePreviewClientProps {
   noteId: string;
@@ -11,13 +13,35 @@ interface NotePreviewClientProps {
 export default function NotePreviewClient({ noteId }: NotePreviewClientProps) {
   const router = useRouter();
 
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['note', noteId],
+    queryFn: () => fetchNoteById(noteId),
+    enabled: Boolean(noteId),
+    refetchOnMount: false,
+  });
+
   if (!noteId) {
     return null;
   }
 
   return (
     <Modal onClose={() => router.back()}>
-      <NotePreview noteId={noteId} />
+      {isLoading && <p>Loading, please wait...</p>}
+      {(error || !note) && !isLoading && <p>Something went wrong.</p>}
+      {note && !isLoading && !error && (
+        <div className={css.item}>
+          <div className={css.header}>
+            <h2>{note.title}</h2>
+          </div>
+          <p className={css.tag}>{note.tag}</p>
+          <p className={css.content}>{note.content}</p>
+          <p className={css.date}>{note.createdAt}</p>
+        </div>
+      )}
     </Modal>
   );
 }
